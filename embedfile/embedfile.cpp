25f2111f92231127b5e7a00ea273abe230c229cc
Dominique Parker <bondscripting@outlook.com>
2013-05-24 23:55:05 -0700

    Trial version of embedfile.cpp. The changes will likely be reverted.

--- embedfile/embedfile.cpp
+++ embedfile/embedfile.cpp
@@ -1,54 +1,90 @@
 #include <ctype.h>
+#include <limits.h>
 #include <stdio.h>
 #include <string.h>
 
+void CopyTemplateHeader(FILE *templateFile, FILE *outputFile, const char *substitutionString)
+{
+	if ((outputFile != NULL) && (templateFile != NULL))
+	{
+		char buffer[LINE_MAX];
+		while ((fgets(buffer, sizeof(buffer), templateFile) != NULL) &&
+		       (strstr(buffer, substitutionString) == NULL))
+		{
+			fprintf(outputFile, "%s", buffer);
+		}
+	}
+}
+
+void CopyTemplateFooter(FILE *templateFile, FILE *outputFile)
+{
+	if ((outputFile != NULL) && (templateFile != NULL))
+	{
+		char buffer[LINE_MAX];
+		while (fgets(buffer, sizeof(buffer), templateFile) != NULL)
+		{
+			fprintf(outputFile, "%s", buffer);
+		}
+	}
+}
+
+
 void EmbedFile(FILE *cppFile, FILE *hFile, const char *inputFileName)
 {
-	const size_t MAX_NAME_LENGTH = 512;
-	char baseName[MAX_NAME_LENGTH];
+	char baseName[LINE_MAX];
 	const char *n = inputFileName;
 	size_t j = 0;
-	while ((*n != '\0') && (j < (MAX_NAME_LENGTH - 1)))
+	while ((*n != '\0') && (j < (LINE_MAX - 1)))
 	{
 		const char c = *n++;
 		baseName[j++] = isalnum(c) ? toupper(c) : '_';
 	}
 	baseName[j] = '\0';
 
-	const unsigned BUFFER_SIZE = 16;
-	unsigned char buffer[BUFFER_SIZE];
-	FILE* inputFile = fopen(inputFileName, "rb");
-
-	fprintf(cppFile, "\nextern const Bond::bu8_t %s_DATA[] =\n{\n", baseName);
-
-	while (!feof(inputFile))
+	if (hFile != NULL)
 	{
-		const size_t numBytes = fread(buffer, sizeof(unsigned char), BUFFER_SIZE, inputFile);
-
-		if (numBytes > 0)
-		{
-			for (size_t i = 0; i < numBytes; ++i)
-			{
-				fprintf(cppFile, "%c0x%02X,", (i > 0) ? ' ' : '\t', buffer[i]);
-			}
-			fprintf(cppFile, "\n");
-		}
+		fprintf(hFile, "\nextern const Bond::FileData %s;\n", baseName);
 	}
 
-	fprintf(cppFile, "};\n\nextern const Bond::FileData %s(%s_DATA, sizeof(%s_DATA));\n", baseName, baseName, baseName);
-	fprintf(hFile, "\nextern const Bond::FileData %s;\n", baseName);
-	fclose(inputFile);
+	if (cppFile != NULL)
+	{
+		const unsigned BUFFER_SIZE = 16;
+		unsigned char buffer[BUFFER_SIZE];
+		FILE *inputFile = fopen(inputFileName, "rb");
+		if (inputFile != NULL)
+		{
+			fprintf(cppFile, "\nextern const Bond::bu8_t %s_DATA[] =\n{\n", baseName);
+
+			while (!feof(inputFile))
+			{
+				const size_t numBytes = fread(buffer, sizeof(unsigned char), BUFFER_SIZE, inputFile);
+
+				if (numBytes > 0)
+				{
+					for (size_t i = 0; i < numBytes; ++i)
+					{
+						fprintf(cppFile, "%c0x%02X,", (i > 0) ? ' ' : '\t', buffer[i]);
+					}
+					fprintf(cppFile, "\n");
+				}
+			}
+
+			fprintf(cppFile, "};\n\nextern const Bond::FileData %s(%s_DATA, sizeof(%s_DATA));\n", baseName, baseName, baseName);
+			fclose(inputFile);
+		}
+	}
 }
+
 
 int main(int argc, const char *argv[])
 {
 	const size_t MAX_ENTRIES = 256;
-	const char *cppFileName = "embeddedfile.cpp";
-	const char *hFileName = "embeddedfile.h";
-	const char *includeFileName = "embeddedfile.h";
-	const char *namespaces[MAX_ENTRIES];
-	size_t numNamespaces = 0;
+	const char *cppFileName = NULL;
+	const char *cppTemplateFileName = NULL;
+	const char *hFileName = NULL;
+	const char *hTemplateFileName = NULL;
 	const char *inputFileNames[MAX_ENTRIES];
+	const char *substitutionString = "#*# EMBED FILES #*#";
 	size_t numInputFiles = 0;
 
 	bool error = false;
@@ -66,6 +102,18 @@ int main(int argc, const char *argv[])
 				error = true;
 			}
 		}
+		if (strcmp(argv[i], "-C") == 0)
+		{
+			if (++i < argc)
+			{
+				cppTemplateFileName = argv[i];
+			}
+			else
+			{
+				fprintf(stderr, "Missing argument to -C\n");
+				error = true;
+			}
+		}
 		else if (strcmp(argv[i], "-h") == 0)
 		{
 			if (++i < argc)
@@ -78,30 +126,27 @@ int main(int argc, const char *argv[])
 				error = true;
 			}
 		}
-		else if (strcmp(argv[i], "-i") == 0)
+		else if (strcmp(argv[i], "-H") == 0)
 		{
 			if (++i < argc)
 			{
-				includeFileName = argv[i];
+				hTemplateFileName = argv[i];
 			}
 			else
 			{
-				fprintf(stderr, "Missing argument to -i\n");
+				fprintf(stderr, "Missing argument to -H\n");
 				error = true;
 			}
 		}
-		else if (strcmp(argv[i], "-n") == 0)
+		else if (strcmp(argv[i], "-s") == 0)
 		{
 			if (++i < argc)
 			{
-				if (numNamespaces < MAX_ENTRIES)
-				{
-					namespaces[numNamespaces++] = argv[i];
-				}
+				substitutionString = argv[i];
 			}
 			else
 			{
-				fprintf(stderr, "Missing argument to -n\n");
+				fprintf(stderr, "Missing argument to -s\n");
 				error = true;
 			}
 		}
@@ -121,48 +166,49 @@ int main(int argc, const char *argv[])
 		return 1;
 	}
 
-	FILE* cppFile = fopen(cppFileName, "w");
-	FILE* hFile = fopen(hFileName, "w");
+	FILE *cppFile = NULL;
+	FILE *cppTemplateFile = NULL;
+	FILE *hFile = NULL;
+	FILE *hTemplateFile = NULL;
 
-	if ((cppFile == NULL) || (hFile == NULL))
+	if (cppFileName != NULL)
 	{
-		return 1;
-	}
-
-	fprintf(cppFile, "#include \"%s\"\n", includeFileName);
-	fprintf(hFile, "#include \"bond/io/fileloader.h\"\n");
-
-	if (numNamespaces > 0)
-	{
-		fprintf(cppFile, "\n");
-		fprintf(hFile, "\n");
-
-		for (size_t i = 0; i < numNamespaces; ++i)
+		cppFile = fopen(cppFileName, "w");
+		if (cppTemplateFileName != NULL)
 		{
-			fprintf(cppFile, "namespace %s\n{\n", namespaces[i]);
-			fprintf(hFile, "namespace %s\n{\n", namespaces[i]);
+			cppTemplateFile = fopen(cppTemplateFileName, "r");
 		}
 	}
+
+	if (hFileName != NULL)
+	{
+		hFile = fopen(hFileName, "w");
+		if (hTemplateFileName != NULL)
+		{
+			hTemplateFile = fopen(hTemplateFileName, "r");
+		}
+	}
+
+	if (hFile != NULL)
+	{
+		fprintf(hFile, "#include \"bond/io/fileloader.h\"\n");
+	}
+
+	CopyTemplateHeader(hTemplateFile, hFile, substitutionString);
+	CopyTemplateHeader(cppTemplateFile, cppFile, substitutionString);
 
 	for (size_t i = 0; i < numInputFiles; ++i)
 	{
 		EmbedFile(cppFile, hFile, inputFileNames[i]);
 	}
 
-	if (numNamespaces > 0)
-	{
-		fprintf(cppFile, "\n");
-		fprintf(hFile, "\n");
-
-		for (size_t i = 0; i < numNamespaces; ++i)
-		{
-			fprintf(cppFile, "}\n");
-			fprintf(hFile, "}\n");
-		}
-	}
+	CopyTemplateFooter(hTemplateFile, hFile);
+	CopyTemplateFooter(cppTemplateFile, cppFile);
 
 	fclose(cppFile);
+	fclose(cppTemplateFile);
 	fclose(hFile);
+	fclose(hTemplateFile);
 
 	return 0;
 }
